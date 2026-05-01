0e20303af5ba8954f2e46e697edf37fa629e2e28
KyleKinsella <t54420027@gmail.com>
2026-05-01 16:53:38 +0100

    did a bit of tidy up

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import "./main.css";
+//import { useState, useEffect } from "react";
+//import "./main.css";
 
 const ROWS = 8;
 const COLS = 8;
@@ -14,34 +14,18 @@ const KNIGHT2 = "knight";
 const ROOK2 = "rook";
 const PAWN = "pawn";
 
-var pieces = [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP2, KNIGHT2, ROOK2, PAWN];
+const pieces = [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP2, KNIGHT2, ROOK2, PAWN];
 
-var board = [
-    [], 
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-];
+var board = [[], [], [], [], [], [], [], []];
 
+// TODO: have - "a,b,c,d,e,f,g,h" at the bottom of the board and at the left hand side of the board have - "8 7 6 5 4 3 2 1"
 function initGameBoard(board, pieces, rows, cols) {
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             if (i === 0 || i === 7) {
                 board[i][j] = pieces[j] + " ";
             }
-            
-            //~ if (i === 6) {
-				//~ board[j][cols] = "8 7 6 5 4 3 2 1" + " ";
-			//~ }
-            
-            //~ if (i === 7) {
-				//~ board[i][rows] = "a b c d e f g h" + " ";
-			//~ }
-            
+                
             if (i === 1 || i === 6) {                
                 for (var pawn = 0; pawn < pieces.length; pawn++) {
                      if (pieces[pawn] === PAWN) {
@@ -58,11 +42,16 @@ function initGameBoard(board, pieces, rows, cols) {
     return board;
 } 
 
+// TODO: make a pawn move from one location to another 
+function movePawn(board, pawnLocation, newPawnLocation, rows, cols) {
+}
+
 function App() {
-    const gameBoard = initGameBoard(board, pieces, ROWS, COLS);
-    
-    return (
-        <div>
+    var gameBoard = initGameBoard(board, pieces, ROWS, COLS);
+    var pawn = movePawn(gameBoard, "a7", "a6", ROWS, COLS);
+        
+    return (    
+		<div>
 			{gameBoard.map((row, i) => (
 				<div key={i}>
 					{row.map((cell, j) => (
