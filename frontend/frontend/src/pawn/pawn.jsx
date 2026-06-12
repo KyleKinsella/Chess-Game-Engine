48965762f890041e597ac39ac2d172e77178a291
KyleKinsella <t54420027@gmail.com>
2026-06-12 22:01:20 +0100

    i have not done these yet for my pawn...

--- frontend/frontend/src/pawn/pawn.jsx
+++ frontend/frontend/src/pawn/pawn.jsx
@@ -48,6 +48,14 @@ class pawn extends ChessPiece {
 		super();
 	}
 	
+	//
+	// Notes: 
+	// 1. can my pawn go forward, if their is a pawn or any piece in front of him?
+	// 2. if my pawn has not moved at all (they have 2 legal moves, on their opening move), but if i have a pawn on my last spot... what does my code do ? 
+	// 	  in this scenario, i only have 1 legal move, until this pawn moves or is captured, then i have 2 legal moves... then the pawn works as normal...
+	// 3. if i have not moved my pawn at all, but the other team has moved into one of my spots (one of my legal moves), i cant move forward, 2 spots, i can only go forward 1 spot.
+	//	  BUT, if the other teams piece moves directly in front of my pawn, i cant go anywhere because im blocked...
+	//
 	getLegalMoves(board, oldPawnLocation, pawnColor) {
 		const row = oldPawnLocation[0];
 		const col = oldPawnLocation[1];
