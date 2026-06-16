9cd80be9bc7839c83359ab6aea517919c8bc6908
KyleKinsella <t54420027@gmail.com>
2026-06-16 14:37:43 +0100

    knight piece can capture other teams pieces!

--- frontend/frontend/src/knight/knight.jsx
+++ frontend/frontend/src/knight/knight.jsx
@@ -10,7 +10,7 @@ class knight extends ChessPiece {
 	// you mentioned to only pass in the board, i belive that we should pass in the oldlocation of the knight that we want to find where that knight can move to, we should not need the board for this i believe....
 	// this is javascripts way of over-riding the parent function of "getLegalMoves(...)".
 	//
-	getLegalMoves(board, oldKnightLocation) {	
+	getLegalMoves(board, oldKnightLocation, color) {	
 		const row = oldKnightLocation[0];
 		const col = oldKnightLocation[1];
 		
@@ -30,15 +30,39 @@ class knight extends ChessPiece {
 
 		for (const move of knightMoves) {
 
-			const newRow = row + move[0];
-			const newCol = col + move[1];
+			const newX = row + move[0];
+			const newY = col + move[1];
 			
 			// TODO: you cannot move to a postion that is occupied by another piece!
-			if (newRow >= 0 && newRow < Utils.ROWS && newCol >= 0 && newCol < Utils.COLS) {
-				if (board[newRow][newCol] === Utils.NULL) {		
-					board[newRow][newCol] = Utils.iCanMoveToHere;
-					legalMoves.push([newRow, newCol]);
-				} 
+			if (newX >= 0 && newX < Utils.ROWS && newY >= 0 && newY < Utils.COLS) {
+				if (board[newX][newY] === Utils.NULL) {		
+					board[newX][newY] = Utils.iCanMoveToHere;
+					legalMoves.push([newX, newY]);
+				} else {
+					switch(color) {
+						case Utils.WHITE:
+							// don't let use capture our own pieces! (not good code for checking each piece, Utils won't let me use the white or black pieces arrays, idk why)...
+							if (board[newX][newY] === Utils.WHITE_PAWN || board[newX][newY] === Utils.WHITE_KNIGHT || board[newX][newY] === Utils.WHITE_ROOK || board[newX][newY] === Utils.WHITE_BISHOP || board[newX][newY] === Utils.WHITE_QUEEN || board[newX][newY] === Utils.WHITE_KING || board[newX][newY] === Utils.WHITE_KNIGHT2 || board[newX][newY] === Utils.WHITE_BISHOP2 || board[newX][newY] === Utils.WHITE_ROOK2) {
+								continue;
+							} else {	
+								board[newX][newY] = Utils.iCanCaptureYou;
+								legalMoves.push([newX, newY]);
+							}
+							
+						break;
+							
+						case Utils.BLACK:
+							// don't let use capture our own pieces! (not good code for checking each piece, Utils won't let me use the white or black pieces arrays, idk why)...
+							if (board[newX][newY] === Utils.BLACK_PAWN || board[newX][newY] === Utils.BLACK_KNIGHT || board[newX][newY] === Utils.BLACK_ROOK || board[newX][newY] === Utils.BLACK_BISHOP || board[newX][newY] === Utils.BLACK_QUEEN || board[newX][newY] === Utils.BLACK_KING || board[newX][newY] === Utils.BLACK_KNIGHT2 || board[newX][newY] === Utils.BLACK_BISHOP2 || board[newX][newY] === Utils.BLACK_ROOK2) {
+								continue;
+							} else {
+								board[newX][newY] = Utils.iCanCaptureYou;
+								legalMoves.push([newX, newY]);
+							}
+							
+						break;
+					}
+				}
 			}
 		}
 		return legalMoves;
