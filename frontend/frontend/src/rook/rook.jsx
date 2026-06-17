0f7e03aec24f369d5840a46f0a6d20f5a47e25c8
KyleKinsella <t54420027@gmail.com>
2026-06-17 17:37:40 +0100

    added piece type parameter for detecting capture of other team pieces

--- frontend/frontend/src/rook/rook.jsx
+++ frontend/frontend/src/rook/rook.jsx
@@ -7,7 +7,7 @@ class rook extends ChessPiece {
 		super();
 	}
 	
-	getLegalMoves(board, oldRookLocation, color) {
+	getLegalMoves(board, oldRookLocation, color, pieceType) {
 		const row = oldRookLocation[0];
 		const col = oldRookLocation[1];
 		
@@ -24,7 +24,7 @@ class rook extends ChessPiece {
 			var newX = row + move[0];
 			var newY = col + move[1];
 			
-			const moves = legalMoves(board, newX, newY, move[0], move[1], color);
+			const moves = legalMoves(board, newX, newY, move[0], move[1], color, pieceType);
 			for (var i = 0; i < moves.length; i++) {
 				legal_Moves.push(moves[i]);
 			}
