0bebbcdcc85ca94188847726860b60b88428d89e
KyleKinsella <t54420027@gmail.com>
2026-06-18 16:53:52 +0100

    i can now click a piece (on the frontend) and the piece moves and shows its legal moves on the board, this resulted in removing my old switch statement and now having a new one, but with some of the old code that i wrote on my old switch statement

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Utils from "./utils.jsx";
 import { initGameBoard, findPieceInBoard, pieceToProcess, movePiece } from "./board/Board.jsx";
 import "./main.css";
 
+import { useEffect, useState } from "react";
+
 // for testing purposes ONLY:
 function freeSpot(board) {
 	board[4][1] = Utils.NULL;
@@ -28,7 +30,7 @@ function App() {
 	const whiteQueen = findPieceInBoard(b, Utils.WHITE_QUEEN);
 	const whitePawns = findPieceInBoard(b, Utils.WHITE_PAWN);
 	const whiteKing = findPieceInBoard(b, Utils.WHITE_KING);
-	
+		
 	// BLACK //
 	const blackKnights = findPieceInBoard(b, Utils.BLACK_KNIGHT);
 	const blackRooks = findPieceInBoard(b, Utils.BLACK_ROOK);
@@ -38,160 +40,134 @@ function App() {
 	const blackKing = findPieceInBoard(b, Utils.BLACK_KING);
 	
 	// 
-	// legal moves for each piece - this updates the board to contain "#####" this is the legal moves for that piece
+	// legal moves for each piece - this updates the board to contain "#####" and "$" this is the legal moves for that piece
 	//
-	const values = ["knight", "rook", "bishop", "queen", "pawn", "king"];
-	var rand = values[Math.floor(Math.random() * values.length)];
-	//~ rand = "";
+	const [selectedPiece, setSelectedPiece] = useState("");
 	
-	const colors = [Utils.WHITE, Utils.BLACK];
-	const whiteOrBlack = colors[Math.floor(Math.random() * colors.length)];
+	useEffect(() => {
+		const clickDiv = (text) => {			
+			setSelectedPiece(text.srcElement.innerText);
+		};
 		
-	switch (rand) {
-	 	case "knight":
-	 		freeSpot(b);
+		const div = document.getElementById("myDiv");
+		div.addEventListener("click", clickDiv, selectedPiece);
+		
+		return () => {
+			div.removeEventListener("click", clickDiv, selectedPiece);
+		};
+	}, []);
+	
+	const white = Utils.WHITE;
+	const black = Utils.BLACK;
+	
+	switch (selectedPiece) {
+		case Utils.WHITE_ROOK:
+			freeSpot(b);
 			
-			switch (whiteOrBlack) {
-				case Utils.WHITE:
-					const whiteProcessKnight = pieceToProcess(b, whiteKnights[0], 1, Utils.WHITE_KNIGHT, whiteOrBlack);
-					console.log(whiteProcessKnight);
-					break;
-				
-				case Utils.BLACK:
-					const blackProcessKnight = pieceToProcess(b, blackKnights[0], 0, Utils.BLACK_KNIGHT, whiteOrBlack);
-					console.log(blackProcessKnight);
-					break;
-			}
-			
-	 		break;
-			
-	 	case "rook":
-	 		freeSpot(b);
-	 		
-			switch (whiteOrBlack) {
-				case Utils.WHITE:
-					const whiteProcessRook = pieceToProcess(b, whiteRooks[0], 1, Utils.WHITE_ROOK, whiteOrBlack);
-					console.log(whiteProcessRook);	
-					break;
-				
-				case Utils.BLACK:
-					const blackProcessRook = pieceToProcess(b, blackRooks[1], 0, Utils.BLACK_ROOK, whiteOrBlack);
-					console.log(blackProcessRook);
-					break;
-			}
-			
+			const whiteProcessRook = pieceToProcess(b, whiteRooks[0], 1, Utils.WHITE_ROOK, white);
+			console.log(whiteProcessRook);	
 			break;
-	 		
-	 	case "bishop":
-	 		freeSpot(b);
-	 		
-	 		switch (whiteOrBlack) {
-				case Utils.WHITE:
-					const whiteProcessBishop = pieceToProcess(b, whiteBishops[0], 1, Utils.WHITE_BISHOP, whiteOrBlack);
-					console.log(whiteProcessBishop);
-					break;
-				
-				case Utils.BLACK:
-					const blackProcessBishop = pieceToProcess(b, blackBishops[1], 0, Utils.BLACK_BISHOP, whiteOrBlack);
-					console.log(blackProcessBishop);
-					break;
-			}
-			
-	 		break;
 		
-	 	case "queen":
-	 		freeSpot(b);
-	 		
-	 		switch (whiteOrBlack) {
-				case Utils.WHITE:
-					const whiteProcessQueen = pieceToProcess(b, whiteQueen[0], 0, Utils.WHITE_QUEEN, whiteOrBlack);
-					console.log(whiteProcessQueen);
-					break;
-				
-				case Utils.BLACK:
-					const blackProcessQueen = pieceToProcess(b, blackQueen[0], 0, Utils.BLACK_QUEEN, whiteOrBlack);
-					console.log(blackProcessQueen);
-					break;
-			}
+		case Utils.BLACK_ROOK:
+			freeSpot(b);
 			
-	 		break;
+			const blackProcessRook = pieceToProcess(b, blackRooks[1], 0, Utils.BLACK_ROOK, black);
+			console.log(blackProcessRook);
+			break;
+		
+		case Utils.WHITE_KNIGHT:
+			freeSpot(b);
 			
-	 	case "pawn":
-			var n = 3;
-			var processMe = [];
-			
-			const down = [0, 1, 2, 3, 4, 5, 6, 7];	
-			const up = [8, 9, 10, 11, 12, 13, 14, 15];
+			const whiteProcessKnight = pieceToProcess(b, whiteKnights[0], 1, Utils.WHITE_KNIGHT, white);
+			console.log(whiteProcessKnight);
+			break;
 
-			switch (whiteOrBlack) {
-				case Utils.WHITE:
-					processMe = whitePawns[n];
-					const whiteProcessPawn = pieceToProcess(b, processMe, n, Utils.WHITE_PAWN, whiteOrBlack);
-					console.log(whiteProcessPawn);
-					
-					for (var i = 0; i < down.length; i++) {
-						if (n === down[i]) {
-							var movePawn = movePiece(b, processMe, whiteProcessPawn[0]);
-							// TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it					
-						} 
-					}
-								
-					for (var i = 0; i < up.length; i++) {
-						if (n === up[i]) {
-							var movePawn = movePiece(b, processMe, whiteProcessPawn[1]);
-							// TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it
-						} 
-					}
-					
-					break;
-						
-				case Utils.BLACK:
-					processMe = blackPawns[n];
-					const blackProcessPawn = pieceToProcess(b, processMe, n, Utils.BLACK_PAWN, whiteOrBlack);
-					console.log(blackProcessPawn);
-						
-					for (var i = 0; i < down.length; i++) {
-						if (n === down[i]) {
-							var movePawn = movePiece(b, processMe, blackProcessPawn[1]);
-							// TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it					
-						} 
-					}
-								
-					for (var i = 0; i < up.length; i++) {
-						if (n === up[i]) {
-							var movePawn = movePiece(b, processMe, blackProcessPawn[0]);
-							// TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it
-						} 
-					}
-					
-					break;
+		case Utils.BLACK_KNIGHT:
+			freeSpot(b);
+			
+			const blackProcessKnight = pieceToProcess(b, blackKnights[0], 0, Utils.BLACK_KNIGHT, black);
+			console.log(blackProcessKnight);
+			break;
+		
+		case Utils.WHITE_BISHOP:
+			freeSpot(b);
+			
+			const whiteProcessBishop = pieceToProcess(b, whiteBishops[0], 1, Utils.WHITE_BISHOP, white);
+			console.log(whiteProcessBishop);
+			break;
+		
+		case Utils.BLACK_BISHOP:
+			freeSpot(b);
+			
+			const blackProcessBishop = pieceToProcess(b, blackBishops[1], 0, Utils.BLACK_BISHOP, black);
+			console.log(blackProcessBishop);
+			break;
+				
+		case Utils.WHITE_QUEEN:
+			freeSpot(b);
+			
+			const whiteProcessQueen = pieceToProcess(b, whiteQueen[0], 0, Utils.WHITE_QUEEN, white);
+			console.log(whiteProcessQueen);
+			break;
+			
+		case Utils.BLACK_QUEEN:
+			freeSpot(b);
+			
+			const blackProcessQueen = pieceToProcess(b, blackQueen[0], 0, Utils.BLACK_QUEEN, black);
+			console.log(blackProcessQueen);
+			break;
+			
+		case Utils.WHITE_PAWN:
+			const down1 = [0, 1, 2, 3, 4, 5, 6, 7];
+			const whiteN = 3;
+			const processMe1 = whitePawns[whiteN];
+							
+			const whiteProcessPawn = pieceToProcess(b, processMe1, whiteN, Utils.WHITE_PAWN, white);
+			console.log(whiteProcessPawn);
+			
+			for (var i = 0; i < down1.length; i++) {
+				if (whiteN === down1[i]) {
+					var movePawn = movePiece(b, processMe1, whiteProcessPawn[0]);
+					// TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it					
+				} 
 			}
 			
 			break;
+		
+		case Utils.BLACK_PAWN:
+			const down = [0, 1, 2, 3, 4, 5, 6, 7];				
+			const blackN = 3;
+			const processMe2 = blackPawns[blackN];
 			
-	 	case "king":
-	 		// TODO: don't move the king into check (aka, danger!) - not an issue for now - due to no team colors, just yet...
-	 		
-	 		switch (whiteOrBlack) {
-				case Utils.WHITE:
-					const whiteProcessKing = pieceToProcess(b, whiteKing[0], 0, Utils.WHITE_KING, whiteOrBlack);
-					console.log(whiteProcessKing);
-					break;
+			const blackProcessPawn = pieceToProcess(b, processMe2, blackN, Utils.BLACK_PAWN, black);
+			console.log(blackProcessPawn);
 				
-				case Utils.BLACK:
-					const blackProcessKing = pieceToProcess(b, blackKing[0], 0, Utils.BLACK_KING, whiteOrBlack);
-					console.log(blackProcessKing);
-					break;
+			for (var i = 0; i < down.length; i++) {
+				if (blackN === down[i]) {
+					var movePawn = movePiece(b, processMe2, blackProcessPawn[1]);
+					//~ // TODO: moving your pawn to its legal move, then re-computing your legal moves (this actually works)! i just need a better way to compute it					
+				} 
 			}
+						
+			break;
 			
-	 		break;
-	 }
-	  
+		// TODO: don't move the king into check (aka, danger!)...
+		case Utils.WHITE_KING:
+			const whiteProcessKing = pieceToProcess(b, whiteKing[0], 0, Utils.WHITE_KING, white);
+			console.log(whiteProcessKing);
+			break;
+		
+		case Utils.BLACK_KING:
+			const blackProcessKing = pieceToProcess(b, blackKing[0], 0, Utils.BLACK_KING, black);
+			console.log(blackProcessKing);
+			break;
+	}
+	
 	return (  
 		<div>			
-			<h3 className="rand">We are processing a <u> {whiteOrBlack} {rand}</u></h3>
-				
-			<div className="board">
+			<h3 className="rand">We are processing a {selectedPiece}</h3>
+						
+			<div className="board" id="myDiv">
 				{b.map((row, i) => (
 					<div key={i}>
 						{row.map((cell, j) => (
