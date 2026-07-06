7c47e4e4aa9f9980f10b9286acc9bd8028e926ac
KyleKinsella <t54420027@gmail.com>
2026-07-06 20:20:13 +0100

    i dont need to search through the board anymore to process a piece, i now have the row and col of where you have clicked, so i can now use this info for processing/moving a piece! Due to this update, my code for re-computing your moves once you have moved has broken, this is what i am fixing at the moment...

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -22,26 +22,6 @@ function App() {
 	const b = initGameBoard(Utils.board, Utils.ROWS, Utils.COLS);
 	
 	// 
-	// find all of the pieces on the board, once the board is created
-	//
-	
-	// WHITE TEAM //
-	const whiteKnights = findPieceInBoard(b, Utils.WHITE_KNIGHT);
-	const whiteRooks = findPieceInBoard(b, Utils.WHITE_ROOK);
-	const whiteBishops = findPieceInBoard(b, Utils.WHITE_BISHOP);
-	const whiteQueen = findPieceInBoard(b, Utils.WHITE_QUEEN);
-	const whitePawns = findPieceInBoard(b, Utils.WHITE_PAWN);
-	const whiteKing = findPieceInBoard(b, Utils.WHITE_KING);
-		
-	// BLACK TEAM //
-	const blackKnights = findPieceInBoard(b, Utils.BLACK_KNIGHT);
-	const blackRooks = findPieceInBoard(b, Utils.BLACK_ROOK);
-	const blackBishops = findPieceInBoard(b, Utils.BLACK_BISHOP);
-	const blackQueen = findPieceInBoard(b, Utils.BLACK_QUEEN);
-	const blackPawns = findPieceInBoard(b, Utils.BLACK_PAWN);
-	const blackKing = findPieceInBoard(b, Utils.BLACK_KING);
-	
-	// 
 	// legal moves for each piece - this updates the board to contain "#####" and "$" this is the legal moves for that piece
 	//
 	const [selectedPiece, setSelectedPiece] = useState("");
@@ -62,93 +42,95 @@ function App() {
 	const white = Utils.WHITE;
 	const black = Utils.BLACK;
 	
+	const [row, setI] = useState(0);
+	const [col, setJ] = useState(0);
+	
+	const coords = [row, col];
+	const rrr = JSON.parse("[" + coords + "]");
+		
 	switch (selectedPiece) {
-		case Utils.WHITE_ROOK:		
+		case Utils.WHITE_ROOK:
+		case Utils.iCanMoveToHere:
+		case Utils.iCanCaptureYou:	
 			//~ freeSpot(b);
-			
-			const whiteRook = pieceToProcess(b, whiteRooks[0], selectedPiece, Utils.WHITE_ROOK, white);
+				
+			const whiteRook = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_ROOK, white);
 			console.log(whiteRook);
 			break;
 						
 		case Utils.BLACK_ROOK:
 			//~ freeSpot(b);
 			
-			const blackRook = pieceToProcess(b, blackRooks[0], selectedPiece, Utils.BLACK_ROOK, black);
+			const blackRook = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_ROOK, black);
 			console.log(blackRook);
 			break;
 		
-		case Utils.WHITE_KNIGHT:		
+		case Utils.WHITE_KNIGHT:	
 			//~ freeSpot(b);
 			
-			const whiteKnight = pieceToProcess(b, whiteKnights[0], selectedPiece, Utils.WHITE_KNIGHT, white);
+			const whiteKnight = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_KNIGHT, white);
 			console.log(whiteKnight);
 			break;
 
 		case Utils.BLACK_KNIGHT:
 			//~ freeSpot(b);
 			
-			const blackKnight = pieceToProcess(b, blackKnights[0], selectedPiece, Utils.BLACK_KNIGHT, black);
+			const blackKnight = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_KNIGHT, black);
 			console.log(blackKnight);
 			break;
 		
 		case Utils.WHITE_BISHOP:	
 			//~ freeSpot(b);
 			
-			const whiteBishop = pieceToProcess(b, whiteBishops[0], selectedPiece, Utils.WHITE_BISHOP, white);
+			const whiteBishop = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_BISHOP, white);
 			console.log(whiteBishop);
 			break;
 		
 		case Utils.BLACK_BISHOP:
 			//~ freeSpot(b);
 			
-			const blackBishop = pieceToProcess(b, blackBishops[0], selectedPiece, Utils.BLACK_BISHOP, black);
+			const blackBishop = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_BISHOP, black);
 			console.log(blackBishop);
 			break;
 			
 		case Utils.WHITE_QUEEN:
 			//~ freeSpot(b);
 			
-			const wQueen = pieceToProcess(b, whiteQueen[0], selectedPiece, Utils.WHITE_QUEEN, white);
+			const wQueen = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_QUEEN, white);
 			console.log(wQueen);
 			break;
 			
 		case Utils.BLACK_QUEEN:
 			//~ freeSpot(b);
 				
-			const bQueen = pieceToProcess(b, blackQueen[0], selectedPiece, Utils.BLACK_QUEEN, black);
+			const bQueen = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_QUEEN, black);
 			console.log(bQueen);
 			break;
 			
 		case Utils.WHITE_PAWN:
-		case Utils.iCanMoveToHere:
-		case Utils.iCanCaptureYou:
-			const whitePawnToProcess = 3;
-			
-			const whitePawn = pieceToProcess(b, whitePawns[whitePawnToProcess], selectedPiece, Utils.WHITE_PAWN, white);
+			const whitePawn = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_PAWN, white);
 			console.log(whitePawn);	
 			break;
 		
-		case Utils.BLACK_PAWN:
-			const blackPawnToProcess = 3;
-			
-			const blackPawn = pieceToProcess(b, blackPawns[blackPawnToProcess], selectedPiece, Utils.BLACK_PAWN, black);
+		case Utils.BLACK_PAWN:			
+			const blackPawn = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_PAWN, black);
 			console.log(blackPawn);
 			break;
 			
 		// TODO: don't move the king into check (aka, danger!)...
 		case Utils.WHITE_KING:
-			const wKing = pieceToProcess(b, whiteKing[0], selectedPiece, Utils.WHITE_KING, white);
+			const wKing = pieceToProcess(b, rrr, selectedPiece, Utils.WHITE_KING, white);
 			console.log(wKing);
 			break;
 		
 		case Utils.BLACK_KING:
-			const bKing = pieceToProcess(b, blackKing[0], selectedPiece, Utils.BLACK_KING, black);
+			const bKing = pieceToProcess(b, rrr, selectedPiece, Utils.BLACK_KING, black);
 			console.log(bKing);
 			break;
 	}
 		
 	return (  
-		<div>	
+		<div>
 			<h3 className="rand">We are processing a {selectedPiece}</h3> 
 			<br/>
 						
@@ -156,13 +138,19 @@ function App() {
 				{b.map((row, i) => (
 					<div key={i}>
 						{row.map((cell, j) => (
-							<div key={j} className={`square ${(i + j) % 2 === 0 ? 'is_white' : 'is_black'} ${(j === 0 || j === 1) ? 'white' : 'black'} ${(j === 2 || j === 3 || j === 4 || j === 5) ? 'middle' : ''}`}>
+							<div key={j} onClick={() => { 
+									setI(i);
+									setJ(j);
+								}} 
+								className={`square ${(i + j) % 2 === 0 ? 'is_white' : 'is_black'} ${(j === 0 || j === 1) ? 'white' : 'black'} ${(j === 2 || j === 3 || j === 4 || j === 5) ? 'middle' : ''}`}>
 								<span>{cell}</span>
 							</div>
 						))}
 					</div>
 				))}       
-			</div>
+			</div>	
+			
+			{row} {col}
 		</div>
 	)
 }
